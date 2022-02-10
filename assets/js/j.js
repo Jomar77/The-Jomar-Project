30770a97c5bd0b03a1ba82fe78d54f333cf132f7
JOMAR NACORDA <jnacorda04@gmail.com>
2022-02-10 18:48:42 +0800

    4th commit

---
+++ assets/js/j.js
@@ -0,0 +1,9 @@
+async function getData(){
+
+const response = await fetch('./duo.json')
+const data =  await response.json()
+const {site_streak} = data
+document.getElementById('myLink').textContent = site_streak;
+}
+
+window.onload = getData();
