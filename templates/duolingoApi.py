922f6062976f978c19623e8ecfbc2bc6003eb397
JOMAR NACORDA <jnacorda04@gmail.com>
2022-02-13 22:07:11 +0800

    Flask deployment

--- templates/duolingoApi.py
+++ templates/duolingoApi.py
@@ -1,5 +1,13 @@
+from flask import Flask
+from flask import render_template
 import duolingo
 import json
+
+app = Flask(__name__)
+@app.route('/')
+def my_form():
+    return render_template("index.html")
+
 
 lingo = duolingo.Duolingo('MojNac','7M36veGfShEWkQh')
 json_file = json.dumps(lingo.get_streak_info(), indent=4)
