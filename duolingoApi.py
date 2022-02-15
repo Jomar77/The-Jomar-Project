bdd0a5345c3613610395ecce3307d6ef510656b5
JOMAR NACORDA <jnacorda04@gmail.com>
2022-02-15 10:23:10 +0800

    change

--- duolingoApi.py
+++ duolingoApi.py
@@ -1,9 +1,9 @@
 
-import duolingo
+import duolingo as dl
 import json
 
 
-lingo = duolingo.Duolingo('MojNac','7M36veGfShEWkQh')
+lingo = dl.Duolingo('MojNac','7M36veGfShEWkQh')
 json_file = json.dumps(lingo.get_streak_info(), indent=4)
 with open("duo.json", "w") as outfile:
     outfile.write(json_file)
