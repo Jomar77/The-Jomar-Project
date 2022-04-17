35f119a9ea4a080689a33147c6635754ac021871
JOMAR NACORDA <jnacorda04@gmail.com>
2022-04-17 15:16:45 +0800

    HUMANA RAGYUD

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, render_template, request
 import mysql.connector
 
 def sql_connector():
@@ -16,15 +16,20 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:Pisay_2021@
 @app.route('/', methods = ['GET', 'POST'])
 
 def home():
+    
+    mydb, my_cursor = sql_connector()
     if request.method == 'POST':
-        mydb, my_cursor = sql_connector()
         my_cursor.execute("CREATE TABLE IF NOT EXISTS mydatabase.mytable (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), email VARCHAR(255), message VARCHAR(255))")
         my_cursor.execute("INSERT INTO mydatabase.mytable (name, email, message) VALUES (%s, %s, %s)", (request.form['name'], request.form['email'], request.form['message']))
         mydb.commit()
-        my_cursor.close()
-        mydb.close()
+    
+    my_cursor.execute("SELECT * FROM mydatabase.mytable")
+    data = my_cursor.fetchall() 
+    return render_template('index.html', data = data)
 
-    return render_template("index.html")
+@app.route('/blogs.html', methods = ['GET', 'POST'])
+def blogs():
+    return render_template("blogs.html")
 
 
 if __name__ == '__main__':
