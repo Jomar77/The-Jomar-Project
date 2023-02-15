0f5a3ddb546d785f1518b8b3226e666ff17bba7b
JOMAR NACORDA <jnacorda04@gmail.com>
2023-02-16 12:08:29 +1300

    imma change

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask, render_template, request
 
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
 app = Flask(__name__)
+
+
+
+
 
 @app.route('/', methods = ['GET', 'POST'])
 def home():  
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
     return render_template('index.html')
 
 @app.route('/2021.html', methods = ['GET', 'POST'])
