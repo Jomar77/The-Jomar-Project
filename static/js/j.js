08569d3d36add12f5aee06d9fef2c61e5444dae8
JOMAR NACORDA <jnacorda04@gmail.com>
2023-02-24 20:05:27 +1300

    renewed everything

--- static/js/j.js
+++
@@ -1,9 +0,0 @@
-async function getData(){
-
-const response = await fetch('duo.json')
-const data =  await response.json()
-const {site_streak} = data
-document.getElementById('myLink').textContent = site_streak;
-}
-
-window.onload = getData();
